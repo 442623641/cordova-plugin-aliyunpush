f24dcf81c7906a381f55e40599b10518e1294e63
LeoZhang <442623641@qq.com>
2019-06-11 17:32:43 +0800

    Update PushApplication.java

--- src/android/PushApplication.java
+++ src/android/PushApplication.java
@@ -3,10 +3,6 @@ package com.alipush;
 import android.app.Application;
 
 import android.content.pm.PackageManager;
-
-import com.septnet.check.utils.TeacherCheckApp;
-
-import static com.alipush.PushUtils.getCurProcessName;
 import static com.alipush.PushUtils.initPushService;
 
 public class PushApplication extends Application {
@@ -14,10 +10,6 @@ public class PushApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        if (getApplicationInfo().packageName.equals(getCurProcessName(getApplicationContext()))) {
-            TeacherCheckApp.getInstance().init(this);
-        }
         try {
             initPushService(this);
         } catch (PackageManager.NameNotFoundException e) {
